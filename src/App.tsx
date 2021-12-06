7d7b366d74384ce07874b422dcb3919be6664a13
Ivan <adamovskij00@mail.ru>
2021-12-06 22:17:30 +0300

    add data for Todolist and typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import './App.css';
+import TodoList from "./components/ToDoList";
 
+export type TaskType = {
+  title?: string
+  isDone?: boolean
+  id?: number
+}
+
+// CRUD
 function App() {
+  // BLL
+  const todoListTitle_1: string = "What to do";
+  const todoListTitle_2: string = "What to buy";
+  const tasks_1: Array<TaskType> = [
+    {id: 1, title: "HTML", isDone: false},
+    {id: 2, title: "CSS", isDone: false},
+    {id: 3, title: "JS/TS", isDone: true},
+  ];
+
+  const tasks_2: Array<TaskType> = [
+    {id: 4, title: "Potato", isDone: false},
+    {id: 5, title: "Beer", isDone: true},
+    {id: 6, title: "Fish", isDone: true},
+  ];
+
+
+  // UI
   return (
     <div className="App">
-      <div>
-        <h3>What to learn</h3>
-        <div>
-          <input/>
-          <button>+</button>
-        </div>
-        <ul>
-          <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-          <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-          <li><input type="checkbox" checked={false}/> <span>React</span></li>
-        </ul>
-        <div>
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
-        </div>
-      </div>
+      <TodoList title={todoListTitle_1} tasks={tasks_1}/>
+      <TodoList title={todoListTitle_2} tasks={tasks_2}/>
     </div>
   );
 }
