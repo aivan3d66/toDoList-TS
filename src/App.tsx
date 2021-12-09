cac17f580a1d59f8de879276687a2f23fdb9f069
Ivan <adamovskij00@mail.ru>
2021-12-09 10:19:51 +0300

    add creating of removeTasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TodoList from "./components/ToDoList";
 import state from "./redux/state";
 
 function App() {
+  let initTasksWeb = state.tasksWeb;
+
+  let arr = useState(initTasksWeb);
+  let tasks = arr[0];
+  let setTasks = arr[1];
+
+  function removeTask(id: number) {
+    let filteredTasks = tasks.filter((t) => t.id !== id);
+    setTasks(filteredTasks)
+  }
+
   return (
     <div className="App">
-      <TodoList title={state.todoListTitle[0]} tasks={state.tasksWeb}/>
-      <TodoList title={state.todoListTitle[1]} tasks={state.tasksBuy}/>
+      <TodoList title={state.todoListTitle[0]}
+                tasks={tasks}
+                removeTask={removeTask}/>
+      {/*<TodoList title={state.todoListTitle[1]}*/}
+      {/*          tasks={state.tasksBuy}*/}
+      {/*          removeTask={removeTask}/>*/}
     </div>
   );
 }
