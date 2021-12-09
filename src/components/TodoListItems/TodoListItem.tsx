1c55f3f008f73e5c3499ba2954bbe9d1e57a9356
Ivan <adamovskij00@mail.ru>
2021-12-09 10:21:25 +0300

    add typing props and removeTask to onClick

--- src/components/TodoListItems/TodoListItem.tsx
+++ src/components/TodoListItems/TodoListItem.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import {TaskType} from "../../redux/state";
 
-export const TodoListItem: React.FC<TaskType> = (props) => {
+type TodoListItemProps = {
+  title: string
+  isDone: boolean
+  id: number
+  removeTask: Function,
+}
+
+export const TodoListItem: React.FC<TodoListItemProps> = (props) => {
   return (
     <li>
       <input type="checkbox" checked={props.isDone}/>
       <span>{props.title}</span>
+      <button onClick={() => props.removeTask(props.id)}>-</button>
     </li>
   )
 }
