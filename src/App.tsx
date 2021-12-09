e6121e157035b7e466e8794e4e88c366c2343904
Ivan <adamovskij00@mail.ru>
2021-12-09 10:34:30 +0300

    add filtration

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,16 @@ import state from "./redux/state";
 function App() {
   let initTasksWeb = state.tasksWeb;
   let [tasks, setTasks] = useState(initTasksWeb);
+  let [filter, setFilter] = useState("all");
+
+  let tasksForTodoList = tasks;
+  if (filter === "completed") {
+    tasksForTodoList = tasks.filter(t => t.isDone);
+  }
+
+  if (filter === "active") {
+    tasksForTodoList = tasks.filter(t => !t.isDone);
+  }
 
   function removeTask(id: number) {
     let filteredTasks = tasks.filter((t) => t.id !== id);
@@ -15,7 +25,7 @@ function App() {
   return (
     <div className="App">
       <TodoList title={state.todoListTitle[0]}
-                tasks={tasks}
+                tasks={tasksForTodoList}
                 removeTask={removeTask}/>
       {/*<TodoList title={state.todoListTitle[1]}*/}
       {/*          tasks={state.tasksBuy}*/}
