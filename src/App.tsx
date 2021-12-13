38afe005b15a0d3a1fa5a0efad093b0cbd637c61
Ivan <adamovskij00@mail.ru>
2021-12-13 20:25:09 +0300

    add rename of initTasksWeb

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import state, {TaskType} from "./redux/state";
 export type FilterValueType = "all" | "completed" | "active";
 
 function App() {
-  let initTasksWeb = state.tasks;
-  let [tasks, setTasks] = useState<Array<TaskType>>(initTasksWeb);
+  let initTasks = state.tasks;
+  let [tasks, setTasks] = useState<Array<TaskType>>(initTasks);
   let [filter, setFilter] = useState<FilterValueType>("all");
 
   function changeFilter(value: FilterValueType) {
