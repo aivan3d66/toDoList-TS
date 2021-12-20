c156979280530cb5a6d571c73fb3bf108084c1eb
Ivan <adamovskij00@mail.ru>
2021-12-20 17:41:55 +0300

    add v1 import from uuid

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,8 +1,11 @@
+import {v1} from "uuid";
+
 // BLL
+
 export type TaskType = {
   title: string
   isDone: boolean
-  id: number
+  id: any
   removeTask?: Function,
 }
 
@@ -14,11 +17,11 @@ export type StateType = {
 const state: StateType = {
   todoListTitle: "What to do",
   tasks: [
-    {id: 1, title: "HTML", isDone: true},
-    {id: 2, title: "CSS", isDone: true},
-    {id: 4, title: "JavaScript", isDone: false},
-    {id: 5, title: "TypeScript", isDone: false},
-    {id: 6, title: "React", isDone: false},
+    {id: v1(), title: "HTML", isDone: true},
+    {id: v1(), title: "CSS", isDone: true},
+    {id: v1(), title: "JavaScript", isDone: false},
+    {id: v1(), title: "TypeScript", isDone: false},
+    {id: v1(), title: "React", isDone: false},
   ],
 }
 
