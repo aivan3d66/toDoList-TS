bce045fd6b4ccbda8ee5be647bf6c034cc73449f
Ivan <adamovskij00@mail.ru>
2021-12-28 10:14:14 +0300

    add FILTERS obj

--- src/common/constants.tsx
+++ src/common/constants.tsx
@@ -2,4 +2,8 @@ const FILTER_ALL: string = "all";
 const FILTER_COMPLETED: string = "completed";
 const FILTER_ACTIVE: string = "active";
 
-export {FILTER_ALL, FILTER_ACTIVE, FILTER_COMPLETED};
+export const FILTERS: Filters = {
+  ALL: 'all',
+  COMPLETED: 'completed',
+  ACTIVE: 'active',
+}
