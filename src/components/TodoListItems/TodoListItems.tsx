f142f7e03fa013546ca62b58913d50d7c66927a7
Ivan <adamovskij00@mail.ru>
2022-01-10 12:11:43 +0300

    add new component TodoListItems

---
+++ src/components/TodoListItems/TodoListItems.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {TaskType} from "../../redux/state";
+import SuperCheckbox from "../../common/super-components/SuperCheckboxComponent/SuperCheckbox";
+import {EditableSpan} from "./EditableSpan/EditableSpan";
+import SuperButton from "../../common/super-components/SuperButton/SuperButton";
+import {ChangeStatus, ChangeTaskTitleType, RemoveTask} from "../../App";
+
+type OnClickHandler = () => void;
+type TodoListItemsPropsType = {
+  tasks: Array<TaskType>,
+  changeStatus: ChangeStatus,
+  changeTaskTitle: ChangeTaskTitleType,
+  removeTask: RemoveTask,
+  todoListID: string,
+}
+
+export const TodoListItems: React.FC<TodoListItemsPropsType> = (
+  {
+    tasks,
+    changeStatus,
+    changeTaskTitle,
+    removeTask,
+    todoListID,
+  }
+) => {
+
+
+  const tasksList = tasks.map((task: TaskType, index: number) => {
+      const getIsDoneClassName = task.isDone ? "is-done" : "";
+      const onClickHandler: OnClickHandler = () => removeTask(todoListID, task.id);
+      const onChangeTitleHandler = (newValue: string) => {
+        changeTaskTitle(todoListID, task.id, newValue)
+      }
+
+      return (
+        <li className={getIsDoneClassName} key={index}>
+          <SuperCheckbox
+            checked={task.isDone}
+            changeStatus={changeStatus}
+            id={task.id}
+            todoListID={todoListID}
+          />
+          <EditableSpan title={task.title} onChange={onChangeTitleHandler}/>
+          <SuperButton onClick={onClickHandler}>
+            -
+          </SuperButton>
+        </li>
+      )
+    }
+  );
+
+  return (
+    <ul className="tasks-list__items">
+      {tasksList}
+    </ul>
+  )
+}
