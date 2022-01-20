e02d8ce56a9ae4921d2874723669c8a5bb560cbf
Ivan <adamovskij00@mail.ru>
2022-01-20 22:07:58 +0300

    add increment children test

--- src/state/userReducer.test.tsx
+++ src/state/userReducer.test.tsx
@@ -9,6 +9,11 @@ test('user reducer should increment only age', () => {
   expect(endState.childrenCount).toBe(2)
 })
 
-// test('user reducer should increment only childrenCount', () => {
-//   const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
-// })
+test('user reducer should increment only childrenCount', () => {
+  const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
+
+  const endState = userReducer(startState, {type: 'INCREMENT-CHILDREN-COUNT'})
+
+  expect(endState.childrenCount).toBe(3)
+  expect(endState.age).toBe(20)
+})
