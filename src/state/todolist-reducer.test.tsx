5df561f9b394d0ff3499296dfed029a91c875ad8
Ivan <adamovskij00@mail.ru>
2022-01-21 12:05:21 +0300

    add userReducer 'CHANGE-TODOLIST-TITLE' and 'CHANGE-TODOLIST-FILTER' tests

--- src/state/todolist-reducer.test.tsx
+++ src/state/todolist-reducer.test.tsx
@@ -32,6 +32,7 @@ test('correct todolist should be added', () => {
   const todoListId1 = v1();
   const todoListId2 = v1();
   const newTodoListTitle = "New TodoList"
+
   const startState: Array<TodoListsType> = [
     {
       id: todoListId1,
@@ -44,6 +45,7 @@ test('correct todolist should be added', () => {
       filter: FILTERS.ALL,
     },
   ]
+
   const endState = todoListsReducer(startState, {
     type: 'ADD-TODOLIST',
     title: newTodoListTitle
@@ -52,4 +54,64 @@ test('correct todolist should be added', () => {
   expect(endState.length).toBe(3);
   expect(endState[2].title).toBe(newTodoListTitle)
   expect(endState[2].filter).toBe(FILTERS.ALL)
+})
+
+test('correct todolist should change his name', () => {
+  const todoListId1 = v1();
+  const todoListId2 = v1();
+  const newTodoListTitle = "New TodoList"
+
+  const startState: Array<TodoListsType> = [
+    {
+      id: todoListId1,
+      title: "What to learn",
+      filter: FILTERS.ALL,
+    },
+    {
+      id: todoListId2,
+      title: "What to buy",
+      filter: FILTERS.ALL,
+    },
+  ]
+
+  const action = {
+    type: 'CHANGE-TODOLIST-TITLE',
+    id: todoListId2,
+    title: newTodoListTitle
+  }
+
+  const endState = todoListsReducer(startState, action)
+
+  expect(endState[0].title).toBe("What to learn")
+  expect(endState[1].title).toBe(newTodoListTitle)
+})
+
+test('correct filter should be changed', () => {
+  const todoListId1 = v1();
+  const todoListId2 = v1();
+  const newFilter = FILTERS.COMPLETED
+
+  const startState: Array<TodoListsType> = [
+    {
+      id: todoListId1,
+      title: "What to learn",
+      filter: FILTERS.ALL,
+    },
+    {
+      id: todoListId2,
+      title: "What to buy",
+      filter: FILTERS.ALL,
+    },
+  ]
+
+  const action = {
+    type: 'CHANGE-TODOLIST-FILTER',
+    id: todoListId2,
+    filter: newFilter
+  }
+
+  const endState = todoListsReducer(startState, action)
+
+  expect(endState[0].filter).toBe(FILTERS.ALL)
+  expect(endState[1].filter).toBe(newFilter)
 })
