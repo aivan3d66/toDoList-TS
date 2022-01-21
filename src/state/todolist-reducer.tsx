da939e55178138ece6bc701f4383cbfc24bb541b
Ivan <adamovskij00@mail.ru>
2022-01-21 12:21:18 +0300

    add all actions types

--- src/state/todolist-reducer.tsx
+++ src/state/todolist-reducer.tsx
@@ -1,11 +1,27 @@
 import {TodoListsType} from "../redux/state";
 import {v1} from "uuid";
 import {FILTERS} from "../common/constants";
+import {FilterValueType} from "../App";
 
-export type ActionType = {
-  type: string,
-  [key: string]: any,
+export type RemoveTodoListActionType = {
+  type: 'REMOVE-TODOLIST',
+  id: string,
 }
+export type AddTodoListActionType = {
+  type: 'ADD-TODOLIST',
+  title: string,
+}
+export type ChangeTodoListTitleActionType = {
+  type: 'CHANGE-TODOLIST-TITLE',
+  id: string,
+  title: string
+}
+export type ChangeTodoListFilterActionType = {
+  type: 'CHANGE-TODOLIST-FILTER',
+  id: string,
+  filter: FilterValueType,
+}
+type ActionType = RemoveTodoListActionType | AddTodoListActionType | ChangeTodoListTitleActionType | ChangeTodoListFilterActionType
 
 export const todoListsReducer = (state: Array<TodoListsType>, action: ActionType): Array<TodoListsType> => {
   switch (action.type) {
