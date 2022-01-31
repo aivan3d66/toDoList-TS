4aa21923bff3183c304029e719a12e46aa28c725
Ivan <adamovskij00@mail.ru>
2022-01-31 10:30:42 +0300

    add new todolist-tasks-reducer file and tests

---
+++ src/state/tests/todolist-tasks-reducer.test.tsx
@@ -0,0 +1,49 @@
+import {TodoListsType, TodoListTasksType} from "../../redux/state";
+import {addTodoListAC, todoListsReducer} from "../todolist-reducer";
+import {taskReducer} from "../task-reducer";
+
+test('new array should be added when new todolist is added', () => {
+  const startState: TodoListTasksType = {
+    "todolistId1": [
+      { id: "1", title: "CSS", isDone: false },
+      { id: "2", title: "JS", isDone: true },
+      { id: "3", title: "React", isDone: false }
+    ],
+    "todolistId2": [
+      { id: "1", title: "bread", isDone: false },
+      { id: "2", title: "milk", isDone: true },
+      { id: "3", title: "tea", isDone: false }
+    ]
+  };
+
+  const action = addTodoListAC("new todolist");
+
+  const endState = taskReducer(startState, action)
+
+  const keys = Object.keys(endState);
+  const newKey = keys.find(k => k != "todolistId1" && k != "todolistId2");
+  if (!newKey) {
+    throw Error("new key should be added")
+  }
+
+  expect(keys.length).toBe(3);
+  expect(endState[newKey]).toEqual([]);
+});
+
+test('ids should be equals', () => {
+  const startTasksState: TodoListTasksType = {};
+  const startTodolistsState: Array<TodoListsType> = [];
+
+  const action = addTodoListAC("new todolist");
+  const endTasksState = taskReducer(startTasksState, action)
+  const endTodolistsState = todoListsReducer(startTodolistsState, action)
+
+  const keys = Object.keys(endTasksState);
+  const idFromTasks = keys[0];
+  const idFromTodolists = endTodolistsState[0].id;
+
+  expect(idFromTasks).toBe(action.todoListId);
+  expect(idFromTodolists).toBe(action.todoListId);
+});
+
+
