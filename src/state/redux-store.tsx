e984f45a0fc5c7b45f30276c8dae86171878f97b
Ivan <adamovskij00@mail.ru>
2022-01-31 11:32:35 +0300

    add redux-store types

--- src/state/redux-store.tsx
+++ src/state/redux-store.tsx
@@ -2,10 +2,13 @@ import {createStore, combineReducers, Action} from "redux";
 import {taskReducer} from "./task-reducer";
 import {todoListsReducer} from "./todolist-reducer";
 
-type RootReducersType = typeof rootReducer;
-type PropertiesType<T> = T extends {[key: string]: infer U} ? U : never;
-export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesType<T>>
-export type AppStateType = ReturnType<RootReducersType>
+// type RootReducersType = typeof rootReducer;
+// type PropertiesType<T> = T extends {[key: string]: infer U} ? U : never;
+// export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesType<T>>
+// export type BaseThunksType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
+// export type AppStateType = ReturnType<RootReducersType>
+
+// type AppRootState = ReturnType<typeof rootReducer>
 
 const rootReducer = combineReducers({
   tasks: taskReducer,
