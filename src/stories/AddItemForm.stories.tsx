770b2bcc8849f6c418adf53acbc2f8271745535e
Ivan <adamovskij00@mail.ru>
2022-02-18 23:19:44 +0300

    add AddItemFormExample action

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {AddItemForm} from "../components/AddItemForm";
+import {action} from "@storybook/addon-actions";
 
 export default {
   title: 'AddItemForm Component',
   component: AddItemForm
 }
+
+action("Btn 'add' was pressed inside the form");
 
 export const AddItemFormExample = (props: any) => {
   return (
