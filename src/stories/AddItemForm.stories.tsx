5b32aac10538a76d9ebf66768f5bebc2edd4edcb
Ivan <adamovskij00@mail.ru>
2022-02-18 23:14:46 +0300

    add AddItemFormExample

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -1,6 +1,13 @@
+import React from "react";
 import {AddItemForm} from "../components/AddItemForm";
 
-export  default {
-    title: 'AddItemForm Component',
-    component: AddItemForm
+export default {
+  title: 'AddItemForm Component',
+  component: AddItemForm
+}
+
+export const AddItemFormExample = (props: any) => {
+  return (
+    <AddItemForm addTask={(title: string) => alert(title)}/>
+  )
 }
