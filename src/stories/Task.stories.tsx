7bef2b79f166835b27a79ccb04a31e37f64e902d
Ivan <adamovskij00@mail.ru>
2022-02-19 00:18:25 +0300

    add new Task

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import {action} from "@storybook/addon-actions";
 import {Task} from "../components/TodoListItems/Task";
+import { v1 } from "uuid";
+import { Provider } from "react-redux";
+import store from "../state/redux-store";
 
 export default {
   title: 'Task Component',
@@ -11,8 +14,19 @@ const callback = action("Btn 'add' was pressed inside the form");
 
 export const TaskExample = (props: any) => {
   return (
-    <>
-      <Task title={'React'} id={'1'} isDone={true} todoListID={'1'}/>
-    </>
+    <Provider store={store}>
+      <Task
+        title={'React'}
+        id={v1()}
+        isDone={false}
+        todoListID={v1()}
+      />
+      <Task
+        title={'TS'}
+        id={v1()}
+        isDone={true}
+        todoListID={v1()}
+      />
+    </Provider>
   )
 }
