04755ecc4d2260e2cb4f4940e375e8303b4ae9b8
Ivan <adamovskij00@mail.ru>
2022-02-19 13:17:59 +0300

    add fix page http

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -1,7 +1,7 @@
 describe('addItemForm', () => {
   it('base example, visually looks correct', async () => {
-    await page.goto();
-    const image = await page.screenshot('http://localhost:6006/iframe.html?path=/story/project-additemform-component--add-item-form-example');
+    await page.goto('http://localhost:6006/iframe.html?path=/story/project-additemform-component--add-item-form-example');
+    const image = await page.screenshot();
 
     expect(image).toMatchInlineSnapshot();
   })
