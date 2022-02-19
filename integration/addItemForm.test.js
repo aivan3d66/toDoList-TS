2b0381ba0cda103fafc19e1268e8ab03c2230d3f
Ivan <adamovskij00@mail.ru>
2022-02-19 13:46:59 +0300

    add fix page path

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -1,8 +1,10 @@
-describe('addItemForm', () => {
-  it('base example, visually looks correct', async () => {
-    await page.goto('http://localhost:6006/iframe.html?path=/story/project-additemform-component--add-item-form-example');
+describe("addItemForm", () => {
+  it("base example, visually looks correct", async () => {
+    await page.goto(
+      "http://localhost:6006/?path=/story/project-additemform-component--add-item-form-example"
+    );
     const image = await page.screenshot();
 
     expect(image).toMatchInlineSnapshot();
-  })
-})
+  });
+});
