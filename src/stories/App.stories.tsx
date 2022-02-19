6a5613b0eb68d2e8c16a537d5a11006ee2e7e4d9
Ivan <adamovskij00@mail.ru>
2022-02-19 12:12:02 +0300

    add decorator to AppExample

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -1,20 +1,18 @@
 import {action} from "@storybook/addon-actions";
 import React from "react";
 import App from "../App";
-import {Provider} from "react-redux";
-import store from "../state/redux-store";
+import {ReduxStoreProviderDecorator} from "./RedusStoreProviderDecorator";
 
 export default {
   title: 'Project/App Component',
-  component: App
+  component: App,
+  decorators: [ReduxStoreProviderDecorator]
 }
 
 const callback = action("Editable Span value was changed");
 
 export const AppExample = (props: any) => {
   return (
-    <Provider store={store}>
-      <App/>
-    </Provider>
+    <App/>
   )
 }
