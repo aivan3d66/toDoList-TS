b9ecc9a24c8eeaf3109127d40606fa78cf6bf312
Ivan <adamovskij00@mail.ru>
2022-02-19 11:54:25 +0300

    add Provider to AppExample

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -1,6 +1,8 @@
 import {action} from "@storybook/addon-actions";
 import React from "react";
 import App from "../App";
+import {Provider} from "react-redux";
+import store from "../state/redux-store";
 
 export default {
   title: 'Project/App Component',
@@ -11,6 +13,8 @@ const callback = action("Editable Span value was changed");
 
 export const AppExample = (props: any) => {
   return (
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   )
 }
