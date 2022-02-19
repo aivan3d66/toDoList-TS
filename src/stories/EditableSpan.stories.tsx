9b10bdba34f21809c46473fce7622be3d3e36d25
Ivan <adamovskij00@mail.ru>
2022-02-19 11:38:57 +0300

    add EditableSpan story

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {action} from "@storybook/addon-actions";
+import {EditableSpan} from "../components/TodoListItems/EditableSpan/EditableSpan";
+
+export default {
+  title: 'Editable Component',
+  component: EditableSpan
+}
+
+const callback = action("Editable Span was changed");
+
+export const EditableSpanExample = (props: any) => {
+  return (
+    <EditableSpan title={'Span title'} onChange={callback}/>
+  )
+}
