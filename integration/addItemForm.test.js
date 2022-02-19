4a6daa1d0e5fc09cd7d981d42988a77190bc311c
Ivan <adamovskij00@mail.ru>
2022-02-19 14:00:20 +0300

    add fix test bug

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -1,7 +1,7 @@
 describe("addItemForm", () => {
   it("base example, visually looks correct", async () => {
     await page.goto(
-      "http://localhost:6006/?path=/story/project-additemform-component--add-item-form-example"
+      "http://localhost:6006/iframe.html?id=project-additemform-component--add-item-form-example&viewMode=story"
     );
     const image = await page.screenshot();
 
