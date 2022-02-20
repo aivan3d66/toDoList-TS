cdce18192f1a6e0bd3326c84b7ff75bcd6b4c1da
Ivan <adamovskij00@mail.ru>
2022-02-20 16:06:53 +0300

    add task snapshot test

--- integration/task.test.js
+++ integration/task.test.js
@@ -1,1 +1,10 @@
+describe("task", () => {
+  it("task example, visually looks correct", async () => {
+    await page.goto(
+      "http://localhost:6006/iframe.html?id=project-task-component--task-example&viewMode=story"
+    );
+    const image = await page.screenshot();
 
+    expect(image).toMatchImageSnapshot();
+  });
+});
