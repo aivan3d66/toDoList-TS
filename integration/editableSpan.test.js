0047e735ae665d9e7e64e732cee132e8f8a798d2
Ivan <adamovskij00@mail.ru>
2022-02-20 16:04:32 +0300

    add editableSpan snapshot test

--- integration/editableSpan.test.js
+++ integration/editableSpan.test.js
@@ -1,1 +1,10 @@
+describe("editableSpan", () => {
+  it("editableSpan example, visually looks correct", async () => {
+    await page.goto(
+      "http://localhost:6006/iframe.html?id=project-editable-component--editable-span-example&viewMode=story"
+    );
+    const image = await page.screenshot();
 
+    expect(image).toMatchImageSnapshot();
+  });
+});
