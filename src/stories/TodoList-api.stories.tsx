fb1f0699c9ec761e1c11a9367e03193157676aa0
Ivan <adamovskij00@mail.ru>
2022-02-21 22:52:34 +0300

    add GetTodoLists func

---
+++ src/stories/TodoList-api.stories.tsx
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from "react";
+
+export default {
+  title: "Project/API's",
+}
+
+export const GetTodoLists = () => {
+  const [state, setState] = useState(null);
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div>
+      {JSON.stringify(state)}
+    </div>
+  )
+}
