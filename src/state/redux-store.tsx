e10da88e3aa8af411a79a2d3f26faddbb1ab5960
Ivan <adamovskij00@mail.ru>
2022-02-21 23:22:53 +0300

    add store typing

--- src/state/redux-store.tsx
+++ src/state/redux-store.tsx
@@ -4,11 +4,11 @@ import {taskReducer} from "./task-reducer";
 import {todoListsReducer} from "./todolist-reducer";
 import thunkMiddleware, {ThunkAction} from "redux-thunk";
 
-// type RootReducersType = typeof rootReducer;
-// type PropertiesType<T> = T extends {[key: string]: infer U} ? U : never;
-// export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesType<T>>
-// export type BaseThunksType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
-// export type AppStateType = ReturnType<RootReducersType>
+type RootReducersType = typeof rootReducer;
+type PropertiesType<T> = T extends { [key: string]: infer U } ? U : never;
+export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesType<T>>
+export type BaseThunksType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
+export type AppStateType = ReturnType<RootReducersType>
 
 export type AppRootState = ReturnType<typeof rootReducer>
 
