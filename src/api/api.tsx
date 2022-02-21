6b1ea35d9df3e57b32b10b55fab3b97e3800c1a3
Ivan <adamovskij00@mail.ru>
2022-02-21 22:36:19 +0300

    add axios instance

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -11,3 +11,11 @@ export type ApiResponseType<D = {}, RC = ResultCode> = {
 }
 
 const BASE_URL: string = `https://social-network.samuraijs.com/api/1.1`;
+
+export const instance = axios.create({
+  withCredentials: true,
+  baseURL: BASE_URL,
+  headers: {
+    'API-KEY': '326f0c95-3947-488e-9603-2e37b5da986c',
+  }
+})
