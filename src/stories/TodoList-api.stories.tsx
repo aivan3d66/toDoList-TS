236d4913823c4c7cba68c3cf74f6c9d883ee1dc4
Ivan <adamovskij00@mail.ru>
2022-02-21 23:18:43 +0300

    add upd GetTodoLists request

--- src/stories/TodoList-api.stories.tsx
+++ src/stories/TodoList-api.stories.tsx
@@ -1,13 +1,25 @@
 import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 export default {
   title: "Project/API's",
 }
 
+const settings = {
+  withCredentials: true,
+  headers: {
+    'API-KEY': '326f0c95-3947-488e-9603-2e37b5da986c',
+  }
+}
+
 export const GetTodoLists = () => {
   const [state, setState] = useState(null);
   useEffect(() => {
+    let response = axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings);
 
+    response.then((res) => {
+      setState(res.data);
+    })
   }, [])
 
   return (
