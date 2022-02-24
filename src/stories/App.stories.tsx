a7ddcb832e83832a963ec96d0233de40f08fad9a
Ivan <adamovskij00@mail.ru>
2022-02-24 21:15:36 +0300

    add upd AppExample story component

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import App from "../App";
 import {ReduxStoreProviderDecorator} from "./RedusStoreProviderDecorator";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
 
 export default {
   title: 'Project/Components/App Component',
   component: App,
   decorators: [ReduxStoreProviderDecorator]
-}
+} as ComponentMeta<typeof App>
 
-export const AppExample = (props: any) => {
-  return (
-    <App/>
-  )
-}
+const Template: ComponentStory<typeof App> = (args) => <App {...args}/>
+
+export const AppExample = Template.bind({});
+AppExample.args = {}
