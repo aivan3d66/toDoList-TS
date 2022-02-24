8c069a38e551d1b6a588c316cc23606d6ab759af
Ivan <adamovskij00@mail.ru>
2022-02-24 20:58:51 +0300

    add upd Task component

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -2,66 +2,31 @@ import React from "react";
 import {action} from "@storybook/addon-actions";
 import {Task} from "../components/TodoListItems/Task";
 import {v1} from "uuid";
-import {Checkbox, IconButton} from "@mui/material";
-import {EditableSpan} from "../components/TodoListItems/EditableSpan/EditableSpan";
-import DeleteIcon from "@mui/icons-material/Delete";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {ReduxStoreProviderDecorator} from "./RedusStoreProviderDecorator";
 
 export default {
   title: 'Project/Components/Task Component',
-  component: Task
-}
+  component: Task,
+  decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof Task>
 
-const taskStyles = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  padding: "4px 0",
-  margin: "0",
-  fontSize: "14px",
-  borderBottom: "1px solid #bababa",
-}
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args}/>
 
+export const TaskExampleDone = Template.bind({});
+TaskExampleDone.args = {
+  title: "react",
+  isDone: true,
+  id: v1(),
+  todoListID: v1(),
+}
+export const TaskExampleNotDone = Template.bind({});
+TaskExampleNotDone.args = {
+  title: "TypeScript",
+  isDone: false,
+  id: v1(),
+  todoListID: v1(),
+}
 const onCheckboxChangeCallback = action("Checkbox was changed");
 const onSpanChangeCallback = action("Span was changed");
 const onBtnClickCallback = action("Delete btn was clicked");
-
-export const TaskExample = (props: any) => {
-  return (
-    <>
-      <li key={v1()} style={taskStyles}>
-        <Checkbox
-          color={"success"}
-          checked={true}
-          onChange={onCheckboxChangeCallback}
-        />
-        <EditableSpan
-          title={'React'}
-          onChange={onSpanChangeCallback}
-        />
-        <IconButton
-          onClick={onBtnClickCallback}
-          color={'default'}
-          size={'small'}>
-          <DeleteIcon/>
-        </IconButton>
-      </li>
-      <li key={v1()} style={taskStyles}>
-        <Checkbox
-          color={"success"}
-          checked={false}
-          onChange={onCheckboxChangeCallback}
-        />
-        <EditableSpan
-          title={'TypeScript'}
-          onChange={onSpanChangeCallback}
-        />
-        <IconButton
-          onClick={onBtnClickCallback}
-          color={'default'}
-          size={'small'}>
-          <DeleteIcon/>
-        </IconButton>
-      </li>
-    </>
-  )
-}
