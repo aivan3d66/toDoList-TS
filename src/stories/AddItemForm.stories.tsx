e57526d7b1fa8268aa057f12b2fabbdc16d730aa
Ivan <adamovskij00@mail.ru>
2022-02-24 18:37:13 +0300

    add upd default description and create template

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import {AddItemForm} from "../components/AddItemForm";
 import {action} from "@storybook/addon-actions";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
 
 export default {
   title: 'Project/Components/AddItemForm Component',
-  component: AddItemForm
-}
+  component: AddItemForm,
+  argTypes: {
+    addTask: {
+      description: "Btn 'add' was pressed inside the form"
+    },
+  },
+} as ComponentMeta<typeof AddItemForm>
+
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args}/>
 
 const callback = action("Btn 'add' was pressed inside the form");
 
