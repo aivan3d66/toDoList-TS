0959959091999374892056910d412b0c44ccda49
Ivan <adamovskij00@mail.ru>
2022-02-24 21:05:41 +0300

    add EditableSpan story component

--- src/stories/EditableSpan.stories.tsx
+++ src/stories/EditableSpan.stories.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import {action} from "@storybook/addon-actions";
 import {EditableSpan} from "../components/TodoListItems/EditableSpan/EditableSpan";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
 
 export default {
   title: 'Project/Components/Editable Component',
-  component: EditableSpan
-}
+  component: EditableSpan,
+  argTypes: {
+    value: {
+      title: 'Title for double click',
+      description: 'string',
+    },
+    onChange: {
+      description: "Editable Span value was changed"
+    },
+  },
+} as ComponentMeta<typeof EditableSpan>
 
-const callback = action("Editable Span value was changed");
+const Template: ComponentStory<typeof EditableSpan> = (args) => <EditableSpan {...args}/>
 
-export const EditableSpanExample = (props: any) => {
-  return (
-    <>
-      <span style={{fontWeight: "bold", fontSize: "20px"}}>Double click on this text &#8595;</span>
-      <EditableSpan title={'Span title value'} onChange={callback}/>
-    </>
-  )
+export const EditableSpanExample = Template.bind({});
+EditableSpanExample.args = {
+  title: 'Title for double click',
+  onChange: action("Editable Span value was changed")
 }
