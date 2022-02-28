49fcd9ef9eddf8e9f5de349ebced80bd639de98b
Ivan <adamovskij00@mail.ru>
2022-02-28 10:40:38 +0300

    delete TodoListsType

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,14 +1,8 @@
-import {FilterValueType} from "../App";
 
 export type TaskType = {
   title: string
   isDone: boolean
   id: string
-};
-export type TodoListsType = {
-  id: string,
-  title: string,
-  filter: FilterValueType,
 };
 export type TodoListTasksType = {
   [key: string]: Array<TaskType>,
