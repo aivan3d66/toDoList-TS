926355193e1bdac833141ac8d53a4520e5b756d2
Ivan <adamovskij00@mail.ru>
2022-02-28 11:56:51 +0300

    add fix task test (types, imports and start state)

--- src/state/tests/tasks-reducer.test.tsx
+++ src/state/tests/tasks-reducer.test.tsx
@@ -1,21 +1,94 @@
-import {TodoListTasksType} from "../../redux/state";
-import {addTaskAC, changeStatusTaskAC, changeTaskTitleAC, removeTaskAC, taskReducer} from "../task-reducer";
-import {removeTodoListAC} from "../todolist-reducer";
+import {
+  addTaskAC,
+  changeStatusTaskAC,
+  changeTaskTitleAC,
+  removeTaskAC,
+  taskReducer,
+  TodoListTasksType
+} from "../task-reducer";
+import {removeTodoListAC, todoListId1} from "../todolist-reducer";
+import {TaskPriorities, TaskStatuses} from "../../api/tasks-api";
+
+const startState: TodoListTasksType = {
+  "todoListId1": [
+    {
+      id: "1",
+      title: "HTML",
+      status: TaskStatuses.Completed,
+      todolistId: todoListId1,
+      startDate: '',
+      deadline: '',
+      addedDate: '',
+      priority: TaskPriorities.Low,
+      order: 0,
+      description: '',
+    },
+    {
+      id: "2",
+      title: "CSS",
+      status: TaskStatuses.Completed,
+      todolistId: todoListId1,
+      startDate: '',
+      deadline: '',
+      addedDate: '',
+      priority: TaskPriorities.Low,
+      order: 0,
+      description: '',
+    },
+    {
+      id: "3",
+      title: "JavaScript",
+      status: TaskStatuses.Completed,
+      todolistId: todoListId1,
+      startDate: '',
+      deadline: '',
+      addedDate: '',
+      priority: TaskPriorities.Low,
+      order: 0,
+      description: '',
+    }
+  ],
+  "todoListId2": [
+    {
+      id: "1",
+      title: "Coffee",
+      status: TaskStatuses.Completed,
+      todolistId: todoListId1,
+      startDate: '',
+      deadline: '',
+      addedDate: '',
+      priority: TaskPriorities.Low,
+      order: 0,
+      description: '',
+    },
+    {
+      id: "2",
+      title: "New brains",
+      status: TaskStatuses.Completed,
+      todolistId: todoListId1,
+      startDate: '',
+      deadline: '',
+      addedDate: '',
+      priority: TaskPriorities.Low,
+      order: 0,
+      description: '',
+    },
+    {
+      id: "3",
+      title: "React book",
+      status: TaskStatuses.Completed,
+      todolistId: todoListId1,
+      startDate: '',
+      deadline: '',
+      addedDate: '',
+      priority: TaskPriorities.Low,
+      order: 0,
+      description: '',
+    }
+  ],
+}
 
 test('current task should be removed', () => {
-const startState: TodoListTasksType = {
-    "todoListId1": [
-      {id: "1", title: "HTML", isDone: true},
-      {id: "2", title: "CSS", isDone: true},
-      {id: "3", title: "JavaScript", isDone: false},
-    ],
-    "todoListId2": [
-      {id: "1", title: "Coffee", isDone: true},
-      {id: "2", title: "New brains", isDone: false},
-      {id: "3", title: "React book", isDone: false},
-    ]
-  }
-
   const action = removeTaskAC("todoListId2", "2")
 
   const endState = taskReducer(startState, action)
@@ -26,19 +99,6 @@ const startState: TodoListTasksType = {
 test('current task should be added', () => {
   const newTitle = "Whiskey"
 
-  const startState: TodoListTasksType = {
-    "todoListId1": [
-      {id: "1", title: "HTML", isDone: true},
-      {id: "2", title: "CSS", isDone: true},
-      {id: "3", title: "JavaScript", isDone: false},
-    ],
-    "todoListId2": [
-      {id: "1", title: "Coffee", isDone: true},
-      {id: "2", title: "New brains", isDone: false},
-      {id: "3", title: "React book", isDone: false},
-    ]
-  }
-
   const action = addTaskAC("todoListId2", newTitle)
 
   const endState = taskReducer(startState, action)
@@ -47,24 +107,10 @@ test('current task should be added', () => {
   expect(endState["todoListId2"].length).toBe(4);
   expect(endState["todoListId2"][0].id).toBeDefined();
   expect(endState["todoListId2"][0].title).toBe(newTitle);
-  expect(endState["todoListId2"][0].isDone).toBe(false);
 })
 
 test('current task should change his name', () => {
   const newTitle = "SASS or LESS"
-
-  const startState: TodoListTasksType = {
-    "todoListId1": [
-      {id: "1", title: "HTML", isDone: true},
-      {id: "2", title: "CSS", isDone: true},
-      {id: "3", title: "JavaScript", isDone: false},
-    ],
-    "todoListId2": [
-      {id: "1", title: "Coffee", isDone: true},
-      {id: "2", title: "New brains", isDone: false},
-      {id: "3", title: "React book", isDone: false},
-    ]
-  }
 
   const action = changeTaskTitleAC("todoListId2", "3", newTitle)
 
@@ -74,41 +120,16 @@ test('current task should change his name', () => {
 })
 
 test('current task checkbox should be changed', () => {
-  const newIsDone = false
-
-  const startState: TodoListTasksType = {
-    "todoListId1": [
-      {id: "1", title: "HTML", isDone: true},
-      {id: "2", title: "CSS", isDone: true},
-      {id: "3", title: "JavaScript", isDone: false},
-    ],
-    "todoListId2": [
-      {id: "1", title: "Coffee", isDone: true},
-      {id: "2", title: "New brains", isDone: false},
-      {id: "3", title: "React book", isDone: false},
-    ]
-  }
+  const newIsDone = !TaskStatuses.Completed
 
   const action = changeStatusTaskAC("todoListId1", "1", newIsDone)
+
   const endState = taskReducer(startState, action)
 
-  expect(endState["todoListId1"][0].isDone).toBe(newIsDone)
+  expect(endState["todoListId1"][0].status).toBe(newIsDone)
 })
 
 test('property with todolistId should be deleted', () => {
-  const startState: TodoListTasksType = {
-    "todolistId1": [
-      { id: "1", title: "CSS", isDone: false },
-      { id: "2", title: "JS", isDone: true },
-      { id: "3", title: "React", isDone: false }
-    ],
-    "todolistId2": [
-      { id: "1", title: "bread", isDone: false },
-      { id: "2", title: "milk", isDone: true },
-      { id: "3", title: "tea", isDone: false }
-    ]
-  };
-
   const action = removeTodoListAC("todolistId2");
 
   const endState = taskReducer(startState, action)
