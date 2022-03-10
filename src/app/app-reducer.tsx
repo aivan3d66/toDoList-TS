378d485eb12e6dbebeab23fe971a01577ae6ca6c
Ivan <adamovskij00@mail.ru>
2022-03-10 11:46:44 +0300

    add appReducer ActionType

--- src/app/app-reducer.tsx
+++ src/app/app-reducer.tsx
@@ -1,9 +1,26 @@
 type InitialStateType = {
   status: 'idle' | 'loading' | 'succeeded' | 'failed',
   error: string | null,
-}
+};
+type ActionType = any;
+
+const SET_STATUS = 'APP/SET-STATUS';
+const SET_ERROR = 'APP/SET-ERROR';
 
 const initialState: InitialStateType = {
   status: 'idle',
   error: null,
+};
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
+  switch (action.type) {
+    case SET_STATUS:
+      return {...state, status: action.status}
+
+    case SET_ERROR:
+      return {...state, error: action.error}
+
+    default:
+      return {...state}
+  }
 }
