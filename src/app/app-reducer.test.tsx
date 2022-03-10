4562169ade6d84860393d1b75f003fcf347d2edd
Ivan <adamovskij00@mail.ru>
2022-03-10 13:18:01 +0300

    add appReducer test file

---
+++ src/app/app-reducer.test.tsx
@@ -0,0 +1,15 @@
+import {InitialStateType} from "./app-reducer";
+
+let startState: InitialStateType;
+
+beforeEach(() => {
+  startState = {
+    error: null,
+    status: 'idle',
+  }
+})
+
+
+test('error message should be set', () => {
+
+})
