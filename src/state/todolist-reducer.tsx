cbb6e17cef334b75638088ffe17dd3fb36768136
Ivan <adamovskij00@mail.ru>
2022-03-11 09:37:35 +0300

    add setAppStatus dispatch to deleteTodoListThunk

--- src/state/todolist-reducer.tsx
+++ src/state/todolist-reducer.tsx
@@ -106,11 +106,13 @@ export const setTodoListsThunk = (title: string): ThunkType => async (dispatch) 
   }
 };
 export const deleteTodoListThunk = (todoListId: string): ThunkType => async (dispatch) => {
+  dispatch(setAppStatus('loading'));
   dispatch(changeTodoListEntityStatus(todoListId, 'loading'))
   try {
     const response = await todoListsAPI.deleteTodoList(todoListId);
     if (response.data.resultCode === ResultCode.Success) {
       dispatch(removeTodoListAC(todoListId));
+      dispatch(setAppStatus('succeeded'));
     }
   } catch (e) {
     console.log(e)
