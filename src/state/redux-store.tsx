7862c9808b34b3c382b33f96f64306b024f7a5f6
Ivan <adamovskij00@mail.ru>
2022-03-11 20:53:18 +0300

    delete unused types

--- src/state/redux-store.tsx
+++ src/state/redux-store.tsx
@@ -4,11 +4,9 @@ import {todoListsReducer} from "./todolist-reducer";
 import thunkMiddleware, {ThunkAction} from "redux-thunk";
 import {appReducer} from "../app/app-reducer";
 
-type RootReducersType = typeof rootReducer;
 type PropertiesType<T> = T extends { [key: string]: infer U } ? U : never;
 export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesType<T>>
-export type BaseThunksType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
-export type AppStateType = ReturnType<RootReducersType>
+export type BaseThunksType<A extends Action, R = Promise<void>> = ThunkAction<R, AppRootState, unknown, A>
 
 export type AppRootState = ReturnType<typeof rootReducer>
 
