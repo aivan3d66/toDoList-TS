cd090dcdf1243c94dbee4026149fca0e729e1722
Ivan <adamovskij00@mail.ru>
2022-03-14 21:04:09 +0300

    add authAPI

---
+++ src/api/auth-api.tsx
@@ -0,0 +1,22 @@
+import {ApiResponseType, instance} from "./api";
+
+type GetAuthResponseType = {
+  id: number,
+  email: string,
+  login: string
+};
+type LoginResponseType = {
+  email: string,
+  password: string,
+  rememberMe: boolean,
+  captcha: string,
+}
+
+export const authAPI = {
+  getAuth() {
+    return instance.get<ApiResponseType<GetAuthResponseType>>(`/auth/me`)
+  },
+  login(email: string, password: string, rememberMe: boolean, captcha: string,) {
+    return instance.post<ApiResponseType<LoginResponseType>>(`/auth/login`, {email, password, rememberMe, captcha})
+  }
+}
