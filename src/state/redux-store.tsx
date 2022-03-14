604e6ddb4ca0aeeddea876c7b82e2ca5edc57035
Ivan <adamovskij00@mail.ru>
2022-03-14 21:05:07 +0300

    add loginReducer to redux store

--- src/state/redux-store.tsx
+++ src/state/redux-store.tsx
@@ -3,6 +3,7 @@ import {taskReducer} from "./task-reducer";
 import {todoListsReducer} from "./todolist-reducer";
 import thunkMiddleware, {ThunkAction} from "redux-thunk";
 import {appReducer} from "../app/app-reducer";
+import {loginReducer} from "./login-reducer";
 
 type PropertiesType<T> = T extends { [key: string]: infer U } ? U : never;
 export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesType<T>>
@@ -14,6 +15,7 @@ const rootReducer = combineReducers({
   tasks: taskReducer,
   todoLists: todoListsReducer,
   app: appReducer,
+  auth: loginReducer,
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
