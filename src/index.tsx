aa1863aab365e53bff88c93fc8d278703f06ec70
Ivan <adamovskij00@mail.ru>
2022-03-14 19:27:27 +0300

    add BrowserRouter import

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import store from './state/redux-store';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
