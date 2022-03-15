6030b477372e146333b53639a9d788d1bb80c26a
Ivan <adamovskij00@mail.ru>
2022-03-15 21:53:33 +0300

    add upd initialApp thunk

--- src/app/app-reducer.tsx
+++ src/app/app-reducer.tsx
@@ -1,13 +1,20 @@
+import {BaseThunksType} from "../state/redux-store";
+import {authAPI} from "../api/auth-api";
+import {ResultCode} from "../api/api";
+import {actions} from "../state/login-reducer";
+
 export type StatusType = 'idle' | 'loading' | 'succeeded' | 'failed';
 export type InitialStateType = {
   status: StatusType,
   error: string | null,
   initialised: boolean
 };
+type SetIsLoggedInType = ReturnType<typeof actions.setIsLoggedIn>;
 export type SetErrorActionType = ReturnType<typeof setAppError>;
 export type SetStatusActionType = ReturnType<typeof setAppStatus>;
 export type SetAppInitActionType = ReturnType<typeof setAppInitialised>;
-type ActionType = SetErrorActionType | SetStatusActionType | SetAppInitActionType;
+type ActionType = SetErrorActionType | SetStatusActionType | SetAppInitActionType | SetIsLoggedInType;
+type ThunkType = BaseThunksType<ActionType>;
 
 const SET_STATUS = 'APP/SET-STATUS';
 const SET_ERROR = 'APP/SET-ERROR';
@@ -35,6 +42,18 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
   }
 }
 
-export const setAppError = (error: string | null) => ({type: SET_ERROR, error} as const)
-export const setAppStatus = (status: StatusType) => ({type: SET_STATUS, status} as const)
-export const setAppInitialised = (value: boolean) => ({type: SET_APP_INITIALISED, value} as const)
+export const setAppError = (error: string | null) => ({type: SET_ERROR, error} as const);
+export const setAppStatus = (status: StatusType) => ({type: SET_STATUS, status} as const);
+export const setAppInitialised = (value: boolean) => ({type: SET_APP_INITIALISED, value} as const);
+
+export const initialApp = (): ThunkType => async (dispatch) => {
+  authAPI.getAuth()
+    .then(response => {
+      if (response.data.resultCode === ResultCode.Success) {
+        dispatch(actions.setIsLoggedIn(true));
+      } else {
+
+      }
+      dispatch(setAppInitialised(true));
+    })
+}
