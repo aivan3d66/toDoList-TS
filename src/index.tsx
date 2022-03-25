a2c7da3ada96bdaa2154a6e97299f91e941ef94a
Ivan <adamovskij00@mail.ru>
2022-03-25 12:19:28 +0300

    add hash router

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import store from './state/redux-store';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App/>
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
