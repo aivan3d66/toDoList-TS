05ed48b117f8befd9f7a42c4f1e2d78d63d7de88
Ivan <adamovskij00@mail.ru>
2022-04-05 22:36:24 +0300

    add upd font-size

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -4,90 +4,91 @@ import {Button, TextField, Grid, Alert, Tooltip} from "@mui/material";
 import {Add} from "@mui/icons-material";
 
 type AddItemFormPropsType = {
-  addTask: (title: string) => void,
-  disabled?: boolean
+    addTask: (title: string) => void,
+    disabled?: boolean
 };
 export type OnKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => void;
 export type OnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => void;
 export type AddTaskHandler = () => void;
 
 export const AddItemForm: React.FC<AddItemFormPropsType> = React.memo(({addTask, disabled = false}) => {
-  console.log('AddItemForm called');
+    console.log('AddItemForm called');
 
-  const addItemListContStyles = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-  }
-  const addItemTextFieldStyles = {
-    flexGrow: 1,
-    width: "100px",
-    margin: "0 10px 0 0",
-    backgroundColor: "white"
-  }
-  const editableSpanStyles = {
-    width: "100%",
-    margin: "10px 0 0 0",
-    padding: "0 10px",
-    height: "34px",
-  }
-
-  const [title, setTitle] = useState<string>("");
-  const [error, setError] = useState<string>("");
-
-  const addItemHandler: AddTaskHandler = () => {
-    if (title.trim() !== "") {
-      addTask(title);
-      setTitle("");
-      setError("");
-    } else {
-      setError(SET_ERROR_NAME);
+    const addItemListContStyles = {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
     }
-  };
-  const onKeyPressHandler: OnKeyPressHandler = (e) => {
-    if (error !== '') {
-      setError('')
+    const addItemTextFieldStyles = {
+        flexGrow: 1,
+        width: "100px",
+        margin: "0 10px 0 0",
+        backgroundColor: "white",
+        fontSize: "14px",
     }
-    if (e.key === "Enter") {
-      addItemHandler();
+    const editableSpanStyles = {
+        width: "100%",
+        margin: "10px 0 0 0",
+        padding: "0 10px",
+        height: "34px",
     }
-  };
-  const onChangeTitleHandler: OnChangeHandler = (e) => {
-    setTitle(e.currentTarget.value);
-    if (error !== '') {
-      setError('');
-    }
-  };
 
-  return (
-    <Grid container style={addItemListContStyles}>
-      <TextField
-        value={title}
-        size={'small'}
-        error={!!error}
-        onKeyPress={onKeyPressHandler}
-        onChange={onChangeTitleHandler}
-        style={addItemTextFieldStyles}
-        disabled={disabled}
-        label="Write the title"
-      />
-      <Tooltip title="Add some title">
-        <Button
-            onClick={addItemHandler}
-            startIcon={<Add/>}
-            variant={'contained'}
-            disabled={disabled}
-        >
-          add
-        </Button>
-      </Tooltip>
+    const [title, setTitle] = useState<string>("");
+    const [error, setError] = useState<string>("");
 
-      {error && <Alert
-        severity="error"
-        variant={"filled"}
-        style={editableSpanStyles}
-      >{SET_ERROR_NAME}</Alert>}
-    </Grid>
-  )
+    const addItemHandler: AddTaskHandler = () => {
+        if (title.trim() !== "") {
+            addTask(title);
+            setTitle("");
+            setError("");
+        } else {
+            setError(SET_ERROR_NAME);
+        }
+    };
+    const onKeyPressHandler: OnKeyPressHandler = (e) => {
+        if (error !== '') {
+            setError('')
+        }
+        if (e.key === "Enter") {
+            addItemHandler();
+        }
+    };
+    const onChangeTitleHandler: OnChangeHandler = (e) => {
+        setTitle(e.currentTarget.value);
+        if (error !== '') {
+            setError('');
+        }
+    };
+
+    return (
+        <Grid container style={addItemListContStyles}>
+            <TextField
+                value={title}
+                size={'small'}
+                error={!!error}
+                onKeyPress={onKeyPressHandler}
+                onChange={onChangeTitleHandler}
+                style={addItemTextFieldStyles}
+                disabled={disabled}
+                label="Write the title"
+            />
+            <Tooltip title="Add some title">
+                <Button
+                    onClick={addItemHandler}
+                    startIcon={<Add/>}
+                    variant={'contained'}
+                    disabled={disabled}
+                >
+                    add
+                </Button>
+            </Tooltip>
+
+            {error && <Alert
+                severity="error"
+                variant={"filled"}
+                style={editableSpanStyles}
+            >{SET_ERROR_NAME}</Alert>}
+        </Grid>
+    )
 })
--- src/features/TodoListLists/TodoList/TodoList.tsx
+++ src/features/TodoListLists/TodoList/TodoList.tsx
@@ -14,130 +14,132 @@ import {TasksResponseType, TaskStatuses} from "../../../api/tasks-api";
 import {TodoListTasks} from "./TodoListItems/TodoListTasks";
 
 export type TodoListProps = {
-  todoList: TodoListDomainType,
-  filter: FilterValueType,
-  changeFilter: ChangeFilter,
-  removeTodoList: RemoveTodoList,
-  changeTodoListTitle: ChangeTodoListTitleType
-  demo?: boolean,
+    todoList: TodoListDomainType,
+    filter: FilterValueType,
+    changeFilter: ChangeFilter,
+    removeTodoList: RemoveTodoList,
+    changeTodoListTitle: ChangeTodoListTitleType
+    demo?: boolean,
 };
 
 const todoListBtnWrapperStyles = {
-  display: "flex",
-  justifyContent: "space-between",
-  margin: "auto 0 0 0"
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "auto 0 0 0",
 }
 
 export const TodoList: React.FC<TodoListProps> = React.memo(({demo = false, ...props}) => {
-    console.log('TodoList called');
+        console.log('TodoList called');
 
-    const {
-      todoList,
-      filter,
-      changeTodoListTitle,
-      removeTodoList,
-      changeFilter,
-    } = props;
+        const {
+            todoList,
+            filter,
+            changeTodoListTitle,
+            removeTodoList,
+            changeFilter,
+        } = props;
 
-    const dispatch = useDispatch();
-    const tasks = useSelector<AppRootState, Array<TasksResponseType>>(state => state.tasks[todoList.id])
+        const dispatch = useDispatch();
+        const tasks = useSelector<AppRootState, Array<TasksResponseType>>(state => state.tasks[todoList.id])
 
-    useEffect(() => {
-      if (demo) {
-        return
-      }
-      dispatch(getAllTodoListTasks({todoListId: todoList.id}))
-    }, [todoList.id])
+        useEffect(() => {
+            if (demo) {
+                return
+            }
+            dispatch(getAllTodoListTasks({todoListId: todoList.id}))
+        }, [todoList.id])
 
-    const onChangeTodoListTitle = useCallback((title: string) => {
-      changeTodoListTitle(todoList.id, title)
-    }, [changeTodoListTitle, todoList.id]);
-    const onRemoveListHandler = () => {
-      removeTodoList(todoList.id);
-    };
+        const onChangeTodoListTitle = useCallback((title: string) => {
+            changeTodoListTitle(todoList.id, title)
+        }, [changeTodoListTitle, todoList.id]);
+        const onRemoveListHandler = () => {
+            removeTodoList(todoList.id);
+        };
 
-    const addTaskHandler = useCallback((title: string) => {
-      dispatch(setNewTodoListTask({todoListId: todoList.id, title}));
-    }, [todoList.id, setNewTodoListTask]);
+        const addTaskHandler = useCallback((title: string) => {
+            dispatch(setNewTodoListTask({todoListId: todoList.id, title}));
+        }, [todoList.id, setNewTodoListTask]);
 
-    let taskForTodoList = tasks;
+        let taskForTodoList = tasks;
 
-    if (filter === FILTERS.ACTIVE) {
-      taskForTodoList = tasks.filter(t => t.status === TaskStatuses.New)
+        if (filter === FILTERS.ACTIVE) {
+            taskForTodoList = tasks.filter(t => t.status === TaskStatuses.New)
+        }
+        if (filter === FILTERS.COMPLETED) {
+            taskForTodoList = tasks.filter(t => t.status === TaskStatuses.Completed)
+        }
+
+        const onAllFilterHandler = useCallback(() => changeFilter(todoList.id, FILTERS.ALL), [todoList.id, FILTERS.ALL, changeFilter]);
+        const onActiveFilterHandler = useCallback(() => changeFilter(todoList.id, FILTERS.ACTIVE), [todoList.id, FILTERS.ACTIVE, changeFilter]);
+        const onCompletedFilterHandler = useCallback(() => changeFilter(todoList.id, FILTERS.COMPLETED), [todoList.id, FILTERS.COMPLETED, changeFilter]);
+
+        const getActiveBtnClassName = (filterValue: FilterValueType) => {
+            return filter === filterValue ? 'contained' : 'outlined';
+        };
+
+        return (
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                minHeight: "400px"
+            }}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    height: "40px",
+                    margin: "0 0 20px 0",
+                    fontSize: "16px",
+                    lineHeight: "16px",
+                    fontWeight: "bold",
+                    fontFamily: "Noto Sans, sans-serif"
+                }}>
+                    <EditableSpan title={todoList.title} onChange={onChangeTodoListTitle}/>
+                    <IconButton
+                        onClick={onRemoveListHandler}
+                        color={'default'}
+                        size={'small'}
+                        disabled={todoList.entityStatus === 'loading'}
+                    >
+                        <DeleteIcon/>
+                    </IconButton>
+                </div>
+                <AddItemForm
+                    addTask={addTaskHandler}
+                    disabled={todoList.entityStatus === 'loading'}
+                />
+
+                <TodoListTasks
+                    tasks={taskForTodoList}
+                    todoListID={todoList.id}
+                />
+
+                <div style={todoListBtnWrapperStyles}>
+                    <Button
+                        variant={getActiveBtnClassName(FILTERS.ALL)}
+                        onClick={onAllFilterHandler}
+                        size={'small'}
+                    >
+                        All
+                    </Button>
+                    <Button
+                        variant={getActiveBtnClassName(FILTERS.ACTIVE)}
+                        onClick={onActiveFilterHandler}
+                        size={'small'}
+                    >
+                        Active
+                    </Button>
+                    <Button
+                        variant={getActiveBtnClassName(FILTERS.COMPLETED)}
+                        onClick={onCompletedFilterHandler}
+                        size={'small'}
+                    >
+                        Completed
+                    </Button>
+                </div>
+            </div>
+        )
     }
-    if (filter === FILTERS.COMPLETED) {
-      taskForTodoList = tasks.filter(t => t.status === TaskStatuses.Completed)
-    }
-
-    const onAllFilterHandler = useCallback(() => changeFilter(todoList.id, FILTERS.ALL), [todoList.id, FILTERS.ALL, changeFilter]);
-    const onActiveFilterHandler = useCallback(() => changeFilter(todoList.id, FILTERS.ACTIVE), [todoList.id, FILTERS.ACTIVE, changeFilter]);
-    const onCompletedFilterHandler = useCallback(() => changeFilter(todoList.id, FILTERS.COMPLETED), [todoList.id, FILTERS.COMPLETED, changeFilter]);
-
-    const getActiveBtnClassName = (filterValue: FilterValueType) => {
-      return filter === filterValue ? 'contained' : 'outlined';
-    };
-
-    return (
-      <div style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        minHeight: "400px"
-      }}>
-        <div style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          height: "40px",
-          margin: "0 0 20px 0",
-          fontSize: "20px",
-          fontWeight: "bold"
-        }}>
-          <EditableSpan title={todoList.title} onChange={onChangeTodoListTitle}/>
-          <IconButton
-            onClick={onRemoveListHandler}
-            color={'default'}
-            size={'small'}
-            disabled={todoList.entityStatus === 'loading'}
-          >
-            <DeleteIcon/>
-          </IconButton>
-        </div>
-        <AddItemForm
-          addTask={addTaskHandler}
-          disabled={todoList.entityStatus === 'loading'}
-        />
-
-        <TodoListTasks
-          tasks={taskForTodoList}
-          todoListID={todoList.id}
-        />
-
-        <div style={todoListBtnWrapperStyles}>
-          <Button
-            variant={getActiveBtnClassName(FILTERS.ALL)}
-            onClick={onAllFilterHandler}
-            size={'small'}
-          >
-            All
-          </Button>
-          <Button
-            variant={getActiveBtnClassName(FILTERS.ACTIVE)}
-            onClick={onActiveFilterHandler}
-            size={'small'}
-          >
-            Active
-          </Button>
-          <Button
-            variant={getActiveBtnClassName(FILTERS.COMPLETED)}
-            onClick={onCompletedFilterHandler}
-            size={'small'}
-          >
-            Completed
-          </Button>
-        </div>
-      </div>
-    )
-  }
 )
