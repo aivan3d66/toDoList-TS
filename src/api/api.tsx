4be72a979189e3d239197e124a402f7a0f5cb1fd
Ivan <adamovskij00@mail.ru>
2022-04-05 21:31:46 +0300

    add fieldsErrors to ApiResponseType

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,23 +1,24 @@
 import axios from "axios";
 
 export enum ResultCode {
-  Success = 0,
-  Error,
+    Success = 0,
+    Error,
 }
 
 export type FieldsErrorsType = Array<{ field: string, error: string }>;
 export type ApiResponseType<D = {}, RC = ResultCode> = {
-  data: D,
-  messages: Array<string>,
-  resultCode: RC
+    data: D,
+    messages: Array<string>,
+    resultCode: RC,
+    fieldsErrors?: FieldsErrorsType
 }
 
 const BASE_URL: string = `https://social-network.samuraijs.com/api/1.1`;
 
 export const instance = axios.create({
-  withCredentials: true,
-  baseURL: BASE_URL,
-  headers: {
-    'API-KEY': '326f0c95-3947-488e-9603-2e37b5da986c',
-  }
+    withCredentials: true,
+    baseURL: BASE_URL,
+    headers: {
+        'API-KEY': '326f0c95-3947-488e-9603-2e37b5da986c',
+    }
 })
