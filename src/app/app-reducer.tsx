0598862c37f9ebedbe0b21c549d8f4ce3233150d
Ivan <adamovskij00@mail.ru>
2022-04-17 21:34:32 +0300

    add app reducer refactoring

--- src/app/app-reducer.tsx
+++ src/app/app-reducer.tsx
@@ -10,38 +10,41 @@ export type InitialStateType = {
   initialised: boolean
 };
 
-export const initialApp = createAsyncThunk('app-slice/initApp', async (arg, thunkAPI) => {
-  return await authAPI.getAuth()
+export const initialApp = createAsyncThunk(
+  'app-slice/initApp',
+  async (arg, thunkAPI) => {
+    return await authAPI.getAuth()
       .then(response => {
         if (response.data.resultCode === ResultCode.Success) {
           thunkAPI.dispatch(setIsLoggedIn({value: true}));
         } else {
 
         }
-        thunkAPI.dispatch(setAppInitialised({value: true}));
       })
-})
+  })
 
-const initialState: InitialStateType = {
-  status: 'idle',
-  error: null,
-  initialised: false
-};
 export const appSlice = createSlice({
   name: 'app-slice',
-  initialState: initialState,
+  initialState: {
+    status: 'idle',
+    error: null,
+    initialised: false
+  } as InitialStateType,
   reducers: {
-    setAppError: (state, action: PayloadAction<{error: string | null}>) => {
+    setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
       state.error = action.payload.error
     },
-    setAppStatus: (state, action: PayloadAction<{status: StatusType}>) => {
+    setAppStatus: (state, action: PayloadAction<{ status: StatusType }>) => {
       state.status = action.payload.status
     },
-    setAppInitialised: (state, action: PayloadAction<{value: boolean}>) => {
-      state.initialised = action.payload.value
-    },
+  },
+  extraReducers: builder => {
+    builder.addCase(initialApp.fulfilled, (state) =>{
+      state.initialised = true
+    })
   }
 });
 
-export const {setAppError, setAppStatus, setAppInitialised} = appSlice.actions;
+export const {setAppError, setAppStatus} = appSlice.actions;
 export const appReducer = appSlice.reducer;
+export const appThunk = {initialApp}
